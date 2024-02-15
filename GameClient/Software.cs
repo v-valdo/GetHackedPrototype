eb8a33f1f854e3c887b26893355b3ced42d60232
v-valdo <v.sersam@gmail.com>
2024-02-15 15:14:17 +0100

    ipscan, attack, status works in client

--- GameClient/Software.cs
+++ GameClient/Software.cs
@@ -1,7 +1,8 @@
 ﻿namespace GameClient;
 using System.Net.Http;
-using System.Net.Http.Headers;
+using System.Net.Mime;
 using System.Text;
+
 public class Software
 {
     public string? Name;
@@ -29,20 +30,69 @@ public class Software
         using StringContent textContent = new StringContent($"{user.Username},{user.Password}", Encoding.UTF8, "text/plain");
         Software IPScanner = new();
         IPScanner.Name = "IPScanner_v1425.exe";
+
         Animation.Loading(IPScanner);
         try
         {
-            using (var requestMessage =
-                        new HttpRequestMessage(HttpMethod.Get, "localhost:3000/ipscanner.exe"))
+            var request = new HttpRequestMessage
             {
-                requestMessage.Headers.Authorization =
-                new AuthenticationHeaderValue(textContent.ToString());
-                await client.SendAsync(requestMessage);
-            }
+                Method = HttpMethod.Get,
+                RequestUri = new($"{uri}ipscanner.exe"),
+                Content = new StringContent($"{user.Username},{user.Password}", Encoding.UTF8, MediaTypeNames.Application.Json /* or "application / json" in older versions */),
+            };
+
+            var response = await client.SendAsync(request).ConfigureAwait(false);
+            response.EnsureSuccessStatusCode();
+
+            var responseBody = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            Console.WriteLine(responseBody);
         }
         catch (Exception e)
         {
             Console.WriteLine(e.Message);
+        }
+    }
+    public static async Task StatusCenter(HttpClient client, Uri uri, User user)
+    {
+        using StringContent textContent = new StringContent($"{user.Username},{user.Password}", Encoding.UTF8, "text/plain");
+        Software StatusCenter = new();
+        StatusCenter.Name = "StatusCenter_V2.exe";
+
+        Animation.Loading(StatusCenter);
+        try
+        {
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new($"{uri}statuscenter.exe"),
+                Content = new StringContent($"{user.Username},{user.Password}", Encoding.UTF8, MediaTypeNames.Application.Json /* or "application / json" in older versions */),
+            };
+
+            var response = await client.SendAsync(request).ConfigureAwait(false);
+            response.EnsureSuccessStatusCode();
+
+            var responseBody = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            Console.WriteLine(responseBody);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+    public static async Task Attack(HttpClient client, Uri uri, User user, string ip)
+    {
+        using StringContent textContent = new StringContent($"{user.Username},{user.Password}", Encoding.UTF8, "text/plain");
+
+        try
+        {
+            using HttpResponseMessage response = await client.PutAsync(client.BaseAddress + $"attack/{ip}", textContent);
+            var jsonResponse = await response.Content.ReadAsStringAsync();
+            Console.WriteLine($"{jsonResponse}\n");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            throw;
         }
     }
 }
